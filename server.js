fa4ca63dbb2b5d1c88e03cb447bfcb22b2a03d4c
Jaegoomon <jaegoo1199@gmail.com>
2021-01-22 14:57:14 +0900

    point에 따라 오름차순으로 정리

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const request = require("request-promise-native");
 const cheerio = require("cheerio");
 const Promise = require("bluebird");
-let link = "https://review7.cre.ma/drgroot.co.kr/products/reviews?app=0&iframe=1&iframe_id=crema-product-reviews-2&nonmember_token=&page=4&parent_url=https%3A%2F%2Fdrgroot.co.kr%2Fproduct%2FBCM%2F157%2F%3Futm_source%3Ddrgroot_bs&utm_medium=naver_bs&utm_campaign=%EC%9E%90%EC%82%AC%EB%AA%B0_%EB%B8%8C%EA%B2%80_%EB%8B%A5%ED%84%B0%EA%B7%B8%EB%A3%A8%ED%8A%B8_PC&utm_term=%EB%9D%BC%EC%9D%B4%ED%8A%B8%ED%98%95&utm_content=%EB%B8%94%EB%9E%91%EC%89%AC%28%EC%82%AC%EC%A0%84%EC%98%88%EC%95%BD%29_%EB%A9%94%EC%9D%B8%ED%85%8D%EC%8A%A4%ED%8A%B8&NaPm=ct%3Dkk7r8ua8%7Cci%3D0zu0002gheDuSp0r1LkL%7Ctr%3Dbrnd%7Chk%3D909780c31c26faa7a1e16eda9dfcae0589ee31ae&product_code=157&secure_device_token=V253f86e388c75199219fd54bc3bb3d992f95f2978f67b66f1b7ea52553c2e693b&widget_env=100&widget_style=";
+const sortJsonArray = require('sort-json-array');
+let link = "https://review7.cre.ma/fila.co.kr/products/reviews?app=0&iframe=1&iframe_id=crema-product-reviews-3&nonmember_token=&page=2&parent_url=https%3A%2F%2Fwww.fila.co.kr%2Fproduct%2Fview.asp%3FProductNo%3D35875&product_code=11001RM01153444&secure_device_token=V249be7fadc8a0f476ec5a44ae71b4ffb1cf17966d4fac349f772b1076c19d72fc&widget_env=100&widget_style=";
 let url = new URL(link);
 
 
@@ -51,7 +52,7 @@ app.get("/results", (req, respond) => {
             }, { concurrency: 100 })
                 .then(results => results.flatMap(result => result))
                 .then(results => {
-                    respond.json(results);
+                    respond.json(sortJsonArray(results, 'point', 'asc'));
                     respond.end();
                 });
         })
