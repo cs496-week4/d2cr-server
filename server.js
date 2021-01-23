a9e503d03f50acd6604e9489e081b7cbacded698
Jaegoomon <jaegoo1199@gmail.com>
2021-01-23 03:38:11 +0000

    post시 DB에 저장

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ const ReviewSchema = new mongoose.Schema({
     date: String
 })
 
+
+
 const Review = mongoose.model("Review", ReviewSchema);
+
+// const UserSchema = new mongoose.Schema({
+//     reviews: Array<Review>
+// })
 
 app.use(bodyParser.json());
 
@@ -47,6 +53,12 @@ app.get('/connect', (req, respond) => {
 
 app.get('/hello', (req, res) => {
     res.send("hello");
+    res.end();
+});
+
+app.get('/delete', (req, res) => {
+    deleteCollection();
+    res.send("Hello")
     res.end();
 });
 
@@ -84,35 +96,36 @@ app.post("/review", (req, respond) => {
             var num = $("span.reviews-count").text();
             var numElem = $("li.review").length;
             console.log("nubmer: ", numElem);
-            return Math.floor(Number(num.replace(/,/g, "")) / numElem) + 1;
+            return Math.ceil(Number(num.replace(/,/g, "")) / numElem);
         })
         .then(res => {
+            console.log(res);
             connectDB("Review");
             let requests = Array.from(Array(res), (_, i) => i);
             return Promise.map(requests, (request) => {
-                return new Promise(resolve => getReviewData(url, request, resolve));
+                return getReviewData(url, request)
+                    .then(res => res[res.length - 1]);
             }, { concurrency: CONCUR_CONSTANT })
-                .then(results => results.flatMap(result => result))
                 .then(results => {
                     // respond.json(results);
                     // respond.json(sortJsonArray(results, 'point', 'asc'));
                     // store in data base
                     console.log("다 끝났다~~~~")
-                    respond.json(results);
+                    respond.send("Hello");
                     return respond.end();
                 })
+                .then(_ => mongoose.connection.close());
         })
         .then(_ => console.log("다 끝났다."))
-        // .then(_ => mongoose.connection.close())
         .catch(err => console.log("request error: ", err));
 });
 
 app.listen(3000);
 
-function getReviewData(link, num, resolve) {
+function getReviewData(link, num) {
     let url = new URL(link);
     url.searchParams.set("page", num);
-    request(url.toString())
+    return request(url.toString())
         .then(res => {
             const $ = cheerio.load(res);
             const block = $(".products_reviews_list_review__inner");
@@ -134,11 +147,11 @@ function getReviewData(link, num, resolve) {
                     user: user,
                     date: date
                 })
-                newReview.save()
-                return { content: content, point: point, pointWord: pointWord, user: user, date: date };
+                return newReview
+                    .save()
+                    .then(_ => console.log("succeed"));
             }).toArray();
         })
-        .then(res => resolve(res))
         .catch(err => console.log("error"));
 }
 
@@ -151,4 +164,21 @@ function connectDB(dataBase) {
         })
         .then(() => console.log("connect to DB"))
         .catch(err => console.error(err));
+}
+
+function deleteCollection() {
+    connectDB("Review")
+        .then(_ => mongoose.connection.db.dropCollection("reviews")
+            .catch(err => console.log("관련 컬렉션 데이터가 없습니다."))
+        )
+        .then(_ => mongoose.connection.close())
+        .catch(err => console.log(err));
+}
+
+function showAscending() {
+    connectDB("Review")
+        .then(_ => {
+            Review.find({},)
+        })
+        .then();
 }
